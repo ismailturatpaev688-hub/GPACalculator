7fbe90c69049baf21a233339223e8087861fbefb
turatpaev-ir <ismailturatpaev688@gmail.com>
2026-06-18 13:39:17 +0700

    Переписана страница с инструкцией и страница о программе

--- GPACalculator/ViewModels/AboutViewModel.cs
+++ GPACalculator/ViewModels/AboutViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class AboutViewModel : BaseViewModel
     {
-        public string AppVersion => "1.0.0";
+        public string AppVersion => "1.2";
         public string Developers => "Андрей Уски, Исмаил Туратпаев";
         public string Group => "Группа 1942с";
         public string Description => "Калькулятор GPA для студентов. " +
